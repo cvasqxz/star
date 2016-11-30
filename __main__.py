df74d96091f933c9d8dba3a0954345a512e1e273
César Vásquez C <cesarvasq@outlook.com>
2016-11-29 23:51:01 -0400

    probando funciones

--- __main__.py
+++ __main__.py
@@ -5,9 +5,11 @@ if __name__ == '__main__':
 	global estrella
 	estrella = [PWMLED(13), PWMLED(16), PWMLED(19), PWMLED(20), PWMLED(21), PWMLED(26)]
 
-	for i in estrella:
-		i.on()
-
 	while True:
-		print 'blip'
-		sleep(1)
+		for i in estrella:
+			i.on()
+			sleep(1)
+
+		for i in estrella:
+			i.off()
+			sleep(1)
